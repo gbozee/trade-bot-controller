fcee89944d719abc3af02a3edffe494b3bb43629
Abiola <gbozee@gmail.com>
2019-10-11 08:38:33 +0100

    To add transaction view
    Transation view would consist of a checkbox to remove specific transaction as well as date, coin, amount traded and profit made.

--- src/components/index.js
+++ src/components/index.js
@@ -1,19 +1,44 @@
-import React from 'react'
-import { Box, Text } from '@chakra-ui/core';
+import React from "react";
+import {
+  Box,
+  Text,
+  Breadcrumb,
+  Icon,
+  BreadcrumbItem,
+  BreadcrumbLink
+} from "@chakra-ui/core";
+import { Link } from "react-router-dom";
 export const NavigationBar = ({ title = "", children }) => {
-    return (
-      <Box
-        bg="tomato"
-        alignItems="center"
-        justifyContent="space-between"
-        w="100%"
-        p={4}
-        color="white"
-        display="flex"
-      >
-        <Text>{title}</Text>
-        {children}
-      </Box>
-    );
-  };
-  
+  return (
+    <Box
+      bg="tomato"
+      alignItems="center"
+      justifyContent="space-between"
+      w="100%"
+      p={4}
+      color="white"
+      display="flex"
+    >
+      <Text>{title}</Text>
+      {children}
+    </Box>
+  );
+};
+
+export const SubNavigationBar = ({ routes, ...rest }) => {
+  return (
+    <Breadcrumb
+      spacing="8px"
+      separator={<Icon color="gray.300" name="chevron-right" />}
+      {...rest}
+    >
+      {routes.map(route => (
+        <BreadcrumbItem key={route.name} isCurrentPage={route.current}>
+          <BreadcrumbLink as={Link} to={route.path}>
+            {route.name}
+          </BreadcrumbLink>
+        </BreadcrumbItem>
+      ))}
+    </Breadcrumb>
+  );
+};
---
+++ src/pages/Login.js
@@ -0,0 +1,50 @@
+import React from "react";
+import {
+  Box,
+  Flex,
+  FormControl,
+  FormLabel,
+  Input,
+  FormErrorMessage,
+  Heading,
+  Button
+} from "@chakra-ui/core";
+import { NavigationBar } from "../components";
+
+export function Login({}) {
+  return (
+    <Box className="App">
+      <NavigationBar title="Bee's Cryptobot | Login" />
+      <Flex
+        flexDirection="column"
+        width={["100%", "80%", "50%"]}
+        margin="0 auto"
+      >
+        <Heading alignSelf="center" py={"1em"}>
+          Login
+        </Heading>
+        <FormControl mb={"1em"} isInvalid={true}>
+          <FormLabel htmlFor="email">Email address</FormLabel>
+          <Input
+            type="email"
+            id="email"
+            placeholder="Email"
+            aria-describedby="email-helper-text"
+          />
+          <FormErrorMessage>Missing email</FormErrorMessage>
+        </FormControl>
+        <FormControl mb={"1em"} isInvalid={true}>
+          <FormLabel htmlFor="email">Password</FormLabel>
+          <Input
+            type="password"
+            id="password"
+            placeholder="Password"
+            aria-describedby="password-helper-text"
+          />
+          <FormErrorMessage>Missing password</FormErrorMessage>
+        </FormControl>
+        <Button variantColor="blue">Button</Button>
+      </Flex>
+    </Box>
+  );
+}
