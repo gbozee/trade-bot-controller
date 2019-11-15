82da0c1b8232d8270781bb0ea5f0f53a1db0f974
Tmosco <you@example.com>
2019-11-15 11:24:25 +0100

    MarketTransaction and MarketAnalyzer

--- src/pages/MarketDetail/MarketAnalyzer.js
+++ src/pages/MarketDetail/MarketAnalyzer.js
@@ -1,7 +1,6 @@
 import React from "react";
 import {
   Box,
-  Flex,
   Input,
   FormControl,
   FormLabel,
@@ -15,98 +14,50 @@ import {
   Button
 } from "@chakra-ui/core";
 
-let textBlob = `------------Profit per trade ---------------\n
-0.042083333333333334\n
-Buy Trades\n
-
-Price: 0.3532   Q:0.31147       Dollar:0.11\n
-Price: 0.3001   Q:0.36652       Dollar:0.11\n
-Price: 0.2471   Q:0.4452        Dollar:0.11\n
-
-
-Sell Trades\n
-
-Price: 0.4592   Q:0.31147       Dollar:0.143\n
-Price: 0.5123   Q:0.36652       Dollar:0.1878\n
-Price: 0.5653   Q:0.4452        Dollar:0.2517\n
-
-
-Config\n\n
-
-buy_amount: 0.11\n
-sell_amount: 0.11\n
-minimum_trades: 10\n
-pair: 3\n
-multiplier: 1\n
-spread_multiplier: 40\n
-_range: 0.001326\n
-equal: quantity\n
-price_places: %.4f\n
-decimal_places: %.5f\n
-
-
-Profit for 3 pairs\n
-0.12625\n
-
-Trades to Complete\n
-397\n
-
-Proposed Budget\n
-43.67\n
-
-Fees in $\n
-0.08734`;
-export const MarketAnalyzer = () => {
-   return (
-    <Flex justifyContent="space-between">
-      <Box display="flex" flex={0.95} flexDirection="column">
-        <Box flexWrap="wrap" display="flex">
-          <FormControl width="42%" mb={1} mx={3} isRequired>
-            <FormLabel htmlFor="market">Market</FormLabel>
-            <Select>
-              <option>USDT</option>
-            </Select>
-          </FormControl>
-          <FormControl mb={1} width="42%" mx={3} isRequired>
-            <FormLabel htmlFor="market">Buy Amount</FormLabel>
-            <Input />
-          </FormControl>
-          <FormControl width="100%" mb={1} mx={3} isRequired>
-            <FormLabel htmlFor="market">Multiplier</FormLabel>
-            <Slider defaultValue={30}>
-              <SliderTrack />
-              <SliderFilledTrack />
-              <SliderThumb size={6}>
-                <Box color="tomato" as={Text} children={4} />
-              </SliderThumb>
-            </Slider>
-          </FormControl>
-          <FormControl width="100%" mb={1} mx={3} isRequired>
-            <FormLabel htmlFor="market">Spread Multiplier</FormLabel>
-            <Slider defaultValue={30}>
-              <SliderTrack />
-              <SliderFilledTrack />
-              <SliderThumb size={6}>
-                <Box color="tomato" as={Text} children={4} />
-              </SliderThumb>
-            </Slider>
-          </FormControl>
-          <Button mb={5}>Submit</Button>
-        </Box>
-        <Code maxHeight={"500px"} overflowY="scroll" pl={2} py={4}>
-          {textBlob.split("\n").map(text => {
-            if (text.trim() === "") {
-              return <br />;
-            }
-            return text;
-          })}
-        </Code>
+export function MarketAnalyzer({ textBlob }) {
+  return (
+    <Box display="flex" flex={0.95} flexDirection="column">
+      <Box flexWrap="wrap" display="flex">
+        <FormControl width="42%" mb={1} mx={3} isRequired>
+          <FormLabel htmlFor="market">Market</FormLabel>
+          <Select>
+            <option>USDT</option>
+          </Select>
+        </FormControl>
+        <FormControl mb={1} width="42%" mx={3} isRequired>
+          <FormLabel htmlFor="market">Buy Amount</FormLabel>
+          <Input />
+        </FormControl>
+        <FormControl width="100%" mb={1} mx={3} isRequired>
+          <FormLabel htmlFor="market">Multiplier</FormLabel>
+          <Slider defaultValue={30}>
+            <SliderTrack />
+            <SliderFilledTrack />
+            <SliderThumb size={6}>
+              <Box color="tomato" as={Text} children={4} />
+            </SliderThumb>
+          </Slider>
+        </FormControl>
+        <FormControl width="100%" mb={1} mx={3} isRequired>
+          <FormLabel htmlFor="market">Spread Multiplier</FormLabel>
+          <Slider defaultValue={30}>
+            <SliderTrack />
+            <SliderFilledTrack />
+            <SliderThumb size={6}>
+              <Box color="tomato" as={Text} children={4} />
+            </SliderThumb>
+          </Slider>
+        </FormControl>
+        <Button mb={5}>Submit</Button>
       </Box>
-    </Flex>
+      <Code maxHeight={"500px"} overflowY="scroll" pl={2} py={4}>
+        {textBlob.split("\n").map(text => {
+          if (text.trim() === "") {
+            return <br />;
+          }
+          return text;
+        })}
+      </Code>
+    </Box>
   );
-};
-
-// get the list of running trades
-// display the completed trades filterable by from and to date
-// ability to determine new spread multiplier or multiplier to use
-// ability to cancel all running trades so that a new one is recreated
+}
--- src/pages/MarketDetail/MarketTransaction.js
+++ src/pages/MarketDetail/MarketTransaction.js
@@ -6,7 +6,7 @@ import {
   Input,
   FormControl,
   FormLabel,
-  Code,
+  Code
 } from "@chakra-ui/core";
 import {
   useTable,
@@ -15,19 +15,6 @@ import {
   useGroupBy,
   useRowSelect
 } from "react-table";
-import matchSorter from "match-sorter";
-import { useNotification } from "../../hooks";
-
-
-// Create an editable cell renderer
-
-function fuzzyTextFilterFn(rows, id, filterValue) {
-  return matchSorter(rows, filterValue, { keys: [row => row.values[id]] });
-}
-
-// Let the table remove the filter if the string is empty
-fuzzyTextFilterFn.autoRemove = val => !val;
-
 function Table({ columns, data, updateMyData, disablePageResetOnDataChange }) {
   // Use the state and functions returned from useTable to build your UI
   const {
@@ -120,9 +107,9 @@ function Table({ columns, data, updateMyData, disablePageResetOnDataChange }) {
         </tbody>
       </Box>
       {/* 
-        Pagination can be built however you'd like. 
-        This is just a very basic UI implementation:
-      */}
+          Pagination can be built however you'd like. 
+          This is just a very basic UI implementation:
+        */}
       <Flex my={3} className="pagination" justifyContent="space-between">
         <Box>
           <IconButton
@@ -154,18 +141,18 @@ function Table({ columns, data, updateMyData, disablePageResetOnDataChange }) {
         </Box>
         <Box display="flex" alignSelf="center">
           {/* <Flex>
-            Go to page:{" "}
-            <Input
-              type="number"
-              size="sm"
-              defaultValue={pageIndex + 1}
-              onChange={e => {
-                const page = e.target.value ? Number(e.target.value) - 1 : 0;
-                gotoPage(page);
-              }}
-              style={{ width: "100px" }}
-            />
-          </Flex> */}
+              Go to page:{" "}
+              <Input
+                type="number"
+                size="sm"
+                defaultValue={pageIndex + 1}
+                onChange={e => {
+                  const page = e.target.value ? Number(e.target.value) - 1 : 0;
+                  gotoPage(page);
+                }}
+                style={{ width: "100px" }}
+              />
+            </Flex> */}
           <select
             value={pageSize}
             onChange={e => {
@@ -181,126 +168,67 @@ function Table({ columns, data, updateMyData, disablePageResetOnDataChange }) {
         </Box>
       </Flex>
       {/* <pre>
-        <code>
-          {JSON.stringify(
-            {
-              pageIndex,
-              pageSize,
-              pageCount,
-              // canNextPage,
-              canPreviousPage,
-              groupBy,
-              expanded,
-              filters,
-              selectedRowPaths
-            },
-            null,
-            2
-          )}
-        </code>
-      </pre> */}
+          <code>
+            {JSON.stringify(
+              {
+                pageIndex,
+                pageSize,
+                pageCount,
+                // canNextPage,
+                canPreviousPage,
+                groupBy,
+                expanded,
+                filters,
+                selectedRowPaths
+              },
+              null,
+              2
+            )}
+          </code>
+        </pre> */}
     </Box>
   );
 }
-export const MarketTransaction = () => {
-    let { messages } = useNotification();
-  const columns = React.useMemo(
-    () => [
-      {
-        id: "selection",
-        // The header can use the table's getToggleAllRowsSelectedProps method
-        // to render a checkbox
-        Header: ({ getToggleAllRowsSelectedProps }) => (
-          <div>
-            <input type="checkbox" {...getToggleAllRowsSelectedProps()} />
-          </div>
-        ),
-        // The cell can use the individual row's getToggleRowSelectedProps method
-        // to the render a checkbox
-        Cell: ({ row }) => (
-          <div>
-            <input type="checkbox" {...row.getToggleRowSelectedProps()} />
-          </div>
-        )
-      },
-      {
-        Header: "Date",
-        accessor: "date"
-      },
-      {
-        Header: "Market",
-        accessor: "market"
-      },
-      {
-        Header: "Amount",
-        accessor: "amount"
-      },
-      {
-        Header: "Profit",
-        accessor: "profit"
-      }
-    ],
-    []
-  );
-  let data = [
-    { date: "2019-10-01", market: "ETHUSDT", amount: 45.323, profit: 2.3 },
-    { date: "2019-10-01", market: "ETHUSDT", amount: 45.323, profit: 2.3 },
-    { date: "2019-10-01", market: "ETHUSDT", amount: 45.323, profit: 2.3 },
-    { date: "2019-10-01", market: "ETHUSDT", amount: 45.323, profit: 2.3 },
-    { date: "2019-10-01", market: "ETHUSDT", amount: 45.323, profit: 2.3 },
-    { date: "2019-10-01", market: "ETHUSDT", amount: 45.323, profit: 2.3 },
-    { date: "2019-10-01", market: "ETHUSDT", amount: 45.323, profit: 2.3 },
-    { date: "2019-10-01", market: "ETHUSDT", amount: 45.323, profit: 2.3 },
-    { date: "2019-10-01", market: "ETHUSDT", amount: 45.323, profit: 2.3 },
-    { date: "2019-10-01", market: "ETHUSDT", amount: 45.323, profit: 2.3 },
-    { date: "2019-10-01", market: "ETHUSDT", amount: 45.323, profit: 2.3 },
-    { date: "2019-10-01", market: "ETHUSDT", amount: 45.323, profit: 2.3 },
-    { date: "2019-10-01", market: "ETHUSDT", amount: 45.323, profit: 2.3 },
-    { date: "2019-10-01", market: "ETHUSDT", amount: 45.323, profit: 2.3 }
-  ];
+
+export function MarketTransaction({ messages, data, columns }) {
   return (
-    <Box px={6}>
-        <Flex direction="column">
-          <Box display="flex" width="15em" mb={3}>
-            <FormControl>
-              <FormLabel>From</FormLabel>
-              <Input size="sm" type="date" placeholder="from" />
-            </FormControl>
-            <FormControl>
-              <FormLabel>to</FormLabel>
-              <Input size="sm" type="date" placeholder="to" />
-            </FormControl>
-            <IconButton alignSelf="flex-end" size="sm" icon="search" />
-          </Box>
-          <Box>
-          <Table columns={columns} data={data} />
-          <Flex direction="column" flex={1} mr={2}>
-           
-              <Code
-                flex={1}
-                width={"100%"}
-                maxHeight={"450px"}
-                height={400}
-                overflowY="scroll"
-                pl={2}
-                py={4}
-              >
-                {messages.map(text => {
-                  if (text.msg) {
-                    return (
-                      <>
-                        {text.msg}
-                        <br />
-                      </>
-                    );
-                  }
-                  return "";
-                })}
-              </Code>
-            </Flex>
-          </Box>
-        </Flex>
-    </Box>
-    
-     );
+    <>
+      <Box display="flex" width="15em" mb={3}>
+        <FormControl>
+          <FormLabel>From</FormLabel>
+          <Input size="sm" type="date" placeholder="from" />
+        </FormControl>
+        <FormControl>
+          <FormLabel>to</FormLabel>
+          <Input size="sm" type="date" placeholder="to" />
+        </FormControl>
+        <IconButton alignSelf="flex-end" size="sm" icon="search" />
+      </Box>
+      <Flex direction="column" flex={1} mr={2}>
+        <Table columns={columns} data={data} />
+
+        <Code
+          flex={1}
+          width={"100%"}
+          maxHeight={"450px"}
+          height={400}
+          overflowY="scroll"
+          pl={2}
+          py={4}
+        >
+          {messages.map(text => {
+            if (text.msg) {
+              return (
+                <>
+                  {text.msg}
+                  <br />
+                </>
+              );
+            }
+            return "";
+          })}
+        </Code>
+      </Flex>
+    </>
+  );
 }
